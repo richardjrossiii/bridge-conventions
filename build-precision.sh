0f2124cf090061f61a77ea016f1689ff22d46b3b
richardjrossiii <richardjrossiii@gmail.com>
2023-12-23 13:50:53 -0800

    Fix build script

--- build-precision.sh
+++ build-precision.sh
@@ -3,6 +3,6 @@
 # link static resources
 rm -rf gen/precision/
 mkdir -p gen/precision/
-(cd gen/precision && ln -s ../../static static)
+cp static/* gen/precision/
 
 dotnet run --project .\Conventor2\Conventor2\Conventor2.csproj -- .\yaml\ .\gen\precision\
