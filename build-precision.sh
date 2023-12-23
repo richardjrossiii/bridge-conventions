37f724c818797f7d03d88af0d098fb9a9959176c
Richard Ross <richardjrossiii@gmail.com>
2023-12-23 14:02:49 -0800

    new build with conventor2

--- build-precision.sh
+++ build-precision.sh
@@ -3,6 +3,6 @@
 # link static resources
 rm -rf gen/precision/
 mkdir -p gen/precision/
-cp static/* gen/precision/
+cp -r static/* gen/precision/
 
-dotnet run --project .\Conventor2\Conventor2\Conventor2.csproj -- .\yaml\ .\gen\precision\
+dotnet run --project ./Conventor2/Conventor2/Conventor2.csproj -- ./yaml ./gen/precision
